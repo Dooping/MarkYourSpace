3dd5ab2c28f4c23963654327f7bb66416780d4c2
David Gago <davidgago@10-22-107-181.ed2.eduroam.fct.unl.pt>
2015-11-02 17:53:25 +0000

    main activity added

--- MarkYourSpace/app/src/main/java/nf/co/markyourspace/markyourspace/LoginActivity.java
+++ MarkYourSpace/app/src/main/java/nf/co/markyourspace/markyourspace/LoginActivity.java
@@ -288,7 +288,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                Intent intent = new Intent(LoginActivity.this, MyReservations.class );
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class );
                 startActivity(intent);
                 //finish();
             } else {
