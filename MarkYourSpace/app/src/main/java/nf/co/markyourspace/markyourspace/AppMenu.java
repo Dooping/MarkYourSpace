9a1031af8862b338c81b3bd624a095bc8aad9e9b
David Gago <davidopg@gmail.com>
2015-11-04 15:20:18 +0000

    Ya esse menu

--- MarkYourSpace/app/src/main/java/nf/co/markyourspace/markyourspace/AppMenu.java
+++ MarkYourSpace/app/src/main/java/nf/co/markyourspace/markyourspace/AppMenu.java
@@ -85,17 +85,17 @@ public class AppMenu extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
+            System.out.println("Ya ta fixe ou que ate se anda bem na vida");
+        } else if (id == R.id.nav_my_buildings) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_find) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_history) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_logout) {
 
         }
 
--- MarkYourSpace/app/src/main/java/nf/co/markyourspace/markyourspace/LoginActivity.java
+++ MarkYourSpace/app/src/main/java/nf/co/markyourspace/markyourspace/LoginActivity.java
@@ -40,7 +40,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     /**
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
-     */
+     */s
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
     };
