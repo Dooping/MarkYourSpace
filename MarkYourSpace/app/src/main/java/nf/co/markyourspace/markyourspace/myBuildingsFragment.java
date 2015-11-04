4175b9177d9e79881f10307f98dc3b605192c425
David Gago <davidopg@gmail.com>
2015-11-04 17:29:26 +0000

    Guilhas apanha isso ai velho

--- MarkYourSpace/app/src/main/java/nf/co/markyourspace/markyourspace/myBuildingsFragment.java
+++ MarkYourSpace/app/src/main/java/nf/co/markyourspace/markyourspace/myBuildingsFragment.java
@@ -64,7 +64,7 @@ public class myBuildingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_buildings2, container, false);
+        return inflater.inflate(R.layout.fragment_my_buildings, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
