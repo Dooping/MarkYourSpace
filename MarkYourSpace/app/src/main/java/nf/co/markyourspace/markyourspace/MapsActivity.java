2eba322ed84468518b073659852ea88ae101d894
David Gago <davidgago@Davids-MacBook-Pro.local>
2015-12-07 21:31:47 +0000

    Release key

--- MarkYourSpace/app/src/main/java/nf/co/markyourspace/markyourspace/MapsActivity.java
+++ MarkYourSpace/app/src/main/java/nf/co/markyourspace/markyourspace/MapsActivity.java
@@ -53,6 +53,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         buildGoogleApiClient();
         mGoogleApiClient.connect();
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
